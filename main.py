d0a69e49f6f557d2a3b7f22f8ec20b39a35299ad
jordanaconnor <jordanaconnor@gmail.com>
2025-09-29 12:57:56 -0600

    bookbot project mvp completed

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-from stats import count_words
-
-frankenstein_path = "books/frankenstein.txt"
+import sys
+print(sys.argv)
+from stats import count_words, count_chars
 
 def get_book_text(path_to_file):
     with open(path_to_file) as f:
@@ -9,9 +9,34 @@ def get_book_text(path_to_file):
     return file_contents
 
 def main():
-    books_text = get_book_text(frankenstein_path)
+
+    if len(sys.argv) == 1:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+    else:
+        book_path = sys.argv[1]
+
+
+
+
+    books_text = get_book_text(book_path)
     count = count_words(books_text)
+    
+    print("============ BOOKBOT ============")
+    print(f"Analyzing book found at {book_path}...")
+    print("----------- Word Count ----------")
     print(f"Found {count} total words")
+    
+    dict_print = count_chars(books_text)
+
+    print("--------- Character Count -------")
+
+    for item in dict_print:
+        if item["name"].isalpha():
+            print(item["name"] + ": " + str(item["num"]))
+
+    print("============= END ===============")
+    
 
 if __name__ == "__main__":
     main()
--- stats.py
+++ stats.py
@@ -1,6 +1,42 @@
+# Dictionary sorter
+def sort_on(items):
+    return items["num"]
+
+# Returns the total count of all words in the provided book.
 def count_words(text):
-    split_text = text.split()
+
     word_counter = 0
+
+    split_text = text.split()
+    
     for word in split_text:
         word_counter +=1
+    
     return word_counter
+
+
+# Returns the total amount of each character in the provided book.
+def count_chars(text):
+    
+    Characters = {}
+    new_list = []    
+    
+    text = text.lower()
+    
+    for word in text:
+        split_word = word.split()
+        
+        for letter in split_word:
+            if letter not in Characters:
+                Characters[letter] = 1
+            else:
+                Characters[letter] += 1
+
+    for key, value in Characters.items():
+        new_dict = {"name": key, "num": value}
+        new_list.append(new_dict)
+    
+    new_list.sort(reverse=True, key=sort_on)
+    return new_list
+
+
